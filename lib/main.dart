d0832a50a6b6e33b24bf652e96b31e386c0839c2
Vivek Vishwakarma <vivek6306619@gmail.com>
2024-03-28 23:42:29 +0530

    Completed NotesApp Project

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Notes',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
